7121fc593b8acb5035e993d55b659e6e4b1df8e6
engine skill <engineskill@enjinnoMacBook-Pro.local>
2023-01-27 10:44:07 +0900

    update/delete

--- prepara.php
+++ prepara.php
@@ -1,17 +1,25 @@
 <?php
 require 'db_conection.php';
 
-$sql = 'SELECT * FROM all_db WHERE id =:id';//SQLのセット :idはプレースホルダー
-$stmt = $pdo->prepare($sql);//SQL文の準備
-$stmt->bindValue('id', 1, PDO::PARAM_INT);//:idと1を紐付ける&&型指定
-$stmt->execute();//SQL実行
-
-
+// $sql = 'SELECT * FROM all_db WHERE id =:id';//SQLのセット :idはプレースホルダー
+// $stmt = $pdo->prepare($sql);//SQL文の準備
+// $stmt->bindValue('id', 1, PDO::PARAM_INT);//:idと1を紐付ける&&型指定
+// $stmt->execute();//SQL実行
 
 // insert文
-$sql = "INSERT INTO `all_db` (`id`, `name`) VALUES (3, 'takuto3')";//SQLのセット :idはプレースホルダー
-$stmt = $pdo->prepare($sql);//SQL文の準備
-$stmt->execute();//SQL実行
+// $sql = "INSERT INTO `all_db` (`id`, `name`) VALUES (3, 'takuto3')";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute();
+
+// update文
+// $sql = "UPDATE `all_db` SET `name` = 'takuto5' WHERE id = 1";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute();
+
+// delete文
+// $sql = "DELETE FROM `all_db` WHERE id = 3;";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute();
 
 //取得
 echo '<pre>';var_dump($stmt->fetchAll());echo '</pre>';exit;
